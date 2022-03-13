c9fffe4b6529f2b20a137b203bf3baf09b409544
Arturo <arturo.antonio.garcia94@gmail.com>
2022-03-13 13:42:24 -0600

    interrupcion serial print

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#define boton 32
+#define led 25
+
+
+void IRAM_ATTR funcioninterrupcion();
 
 void setup() {
-  // put your setup code here, to run once: PRIMER COMMIT, SEGUNDO COMMIT
+
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(boton, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(boton), funcioninterrupcion, FALLING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ Serial.println("Este es un loop");
+
+}
+
+void IRAM_ATTR funcioninterrupcion(){
+
+ Serial.println("botonazo");
+
 }
